94394183c2b2bd4af7350018285afdd6f309bc77
Outron <hipotyzer@gmail.com>
2023-07-27 00:12:04 +0200

    Extend choose_tech function

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ import time
 from selenium.webdriver.common.by import By
 url = 'https://justjoin.it'
 options = webdriver.ChromeOptions()
-options.add_experimental_option("detach", True)
+# options.add_argument("--headless=new") HEADLESS MODE
+options.add_experimental_option("detach", True)  # DETACHT MODE
 driver = webdriver.Chrome(options=options)
 driver.get(url)
 time.sleep(2)
 def click_cookie_button():
-    driver.find_element(By.CSS_SELECTOR, value = "#root > div.jss220.jss221 > button").click()
+    driver.find_element(By.CSS_SELECTOR, value = "#root > div.jss219.jss220 > button").click()
 def night_mode_on():
     driver.find_element(By.CSS_SELECTOR, value = "#root > header > div > div.jss17 > div.jss161 > span > span.MuiButtonBase-root-64.MuiIconButton-root-74.jss183.MuiSwitch-switchBase-176.jss165.MuiSwitch-colorSecondary-178 > span > input").click()
 def choose_offer_type():
@@ -22,26 +23,52 @@ def choose_offer_type():
                 if choose == 2:
                     return driver.find_element(By.CSS_SELECTOR, value="#root > div.css-1smbjja > div.css-kkhecm > div > div.css-p1iqw4 > div.css-1x7tcfa > a.css-1pow96e").click()
             else:
-                print("Incorrect choose")
+                print("Incorrect choice")
                 print("Choose offer type: all offers[1] / Offers with salary[2]:")
         except ValueError:
-            print("Incorrect choose")
+            print("Incorrect choice")
             print("Choose offer type: all offers[1] / Offers with salary[2]:")
 
 def choose_tech():
+    # click on tech selection button,then get list of technologies
     driver.find_element(By.CSS_SELECTOR, value = "#root > div.css-1ho6o7a > div > button:nth-child(3)").click()
-    tech_list = driver.find_element(By.CSS_SELECTOR, value = "body > div.MuiDialog-root > div.MuiDialog-container.MuiDialog-scrollPaper > div > div.MuiDialogContent-root.MuiDialogContent-dividers")
-    tech = tech_list.find_elements(By.XPATH, value = '//a[@aria-disabled="false"]')
-    for t in tech:
-        print(t.text)
+    tech_tab = driver.find_element(By.CSS_SELECTOR, value = "body > div.MuiDialog-root > div.MuiDialog-container.MuiDialog-scrollPaper > div > div.MuiDialogContent-root.MuiDialogContent-dividers")
+    tech_list = tech_tab.find_elements(By.XPATH, value = '//a[@aria-disabled="false"]')
+
+    # Creating a list with names of technologies
+    tech_name = []
+    for index, element in enumerate(tech_list[3:], start=1):
+        tech_name.append(f"{index}.{element.text}")
+
+    #Display names of technologies in colmuns
+    elements_per_column = 5
+    elements_to_display = tech_name[0:]
+    columns = [elements_to_display[i:i + elements_per_column] for i in range(0, len(elements_to_display), elements_per_column)]
+    for i in range(elements_per_column):
+        for col in columns:
+            if i < len(col):
+                element = col[i]
+                print(f"{element:<20}", end="")
+        print()
+
+    # choosing technology
+    while True:
+        try:
+            choose = int(input("Choose technology: (type 1-25) "))
+            name_of_technology = tech_name[abs(choose-1)]
+            tech_list[abs(choose+2)].click()
+            return print("Selected technology: " + name_of_technology)
+        except ValueError:
+            print("Incorrect choice")
 
 
-click_cookie_button()
-time.sleep(1)
+
+#click_cookie_button()
+#time.sleep(1)
 night_mode_on()
 time.sleep(1)
-choose_offer_type()
-time.sleep(1)
+#choose_offer_type()
+#time.sleep(1)
 choose_tech()
 
 
